94ace06b46554839b01219525240c92906b7fb04
fajfara <a.fajfar95@gmail.com>
2018-05-08 18:36:51 +0200

    New day new stuff

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
+    
     return (
       <div className="App">
         <h1>Hi, i am a react app </h1>
+        <Person name="Anze" age="23"/>
+        <Person name="Manu" age="29"/>
+        <Person name="Maruša" age="45">She likes to have sex</Person>
       </div>
-    );
+       );
+      
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'This works?'));
+     
+   
   }
 }
 
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = (props) => {
+    return <p>I'm {this.props.name} and i am {this.props.age} years old</p>
+}
+
+export default person;
